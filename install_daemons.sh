dd8b85c3e130aa7689edeeda37c0d7c50390adcd
Alex Geoffrey <apg@uoregon.edu>
2017-01-12 23:20:15 -0800

    attempt at shell script

--- install_daemons.sh
+++ install_daemons.sh
@@ -1,2 +1,22 @@
-#! usr/bin/bash
+#1 usr/bin/bash
 
+# Alex Geoffrey
+# apg@uoregon.edu
+# CIS 322 Software Engineering
+# Assignment 1
+
+# Clone postgres source code
+git clone https://github.com/postgres/postgres.git
+
+# Configure, make and install postgres
+./configure --prefix="$HOME/installed"
+make
+make install
+
+# Download compressed file
+curl http://apache.spinellicreations.com//httpd/httpd-2.4.25.tar.bz2
+
+# Configure, make and install httpd
+./configure --prefix="$HOME/installed"
+make
+make install
