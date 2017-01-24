9bcd903560bde40750077eb3dd98399636c9356e
Alex Geoffrey <apg@uoregon.edu>
2017-01-24 09:07:56 +0000

    Comp on low battery, must go home and charge

--- sql/import_data.sh
+++ sql/import_data.sh
@@ -7,7 +7,6 @@
 # Curls legacy data for LOST
 #curl https://classes.cs.uoregon.edu//17W/cis322/files/osnap_legacy.tar.gz > legacy_data.tar.gz
 
-
 # Unpack files to /osnap_legacy
 #tar -xvzf legacy_data.tar.gz
 
--- sql/insert_products.py
+++ sql/insert_products.py
@@ -3,20 +3,28 @@ import sys
 import csv
 
 # Connect to the database
-print (sys.argv[0])
-print (sys.argv[1])
-print (sys.argv[2])
-
-conn = psycopg2.connect(dbname=sys.argv[1], host='127.0.0.1', port=int(sys.argv[2]))
+conn = psycopg2.connect(dbname=sys.argv[1], host='/tmp/')
 cur = conn.cursor()
 
+
+
 # Read in data from files
+firstline = True
+
 with open('osnap_legacy/product_list.csv') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
-        cur.execute("INSERT INTO products VALUES (%s)", (row[4] + "," + row[0] + "," + row[2]))
+        if (firstline):
+            firstline = False
+            continue
+        print (row)
+        print (row[4] + ", " + row[0] + "," + row[2])
+        cur.execute("INSERT INTO products (vendor, description, alt_description) VALUES (%s, %s, %s)", (row[4],row[0],row[2]))
 
-print ("\nProduct Inserts complete!\n")
 
+conn.commit()
+
+cur.close()
+conn.close()
 
 
