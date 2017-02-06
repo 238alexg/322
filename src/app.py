88c1101f46ade293849a50d08da20bf322099fed
Alex Geoffrey <apg@uoregon.edu>
2017-02-06 02:35:54 +0000

    Progress on forms

--- src/app.py
+++ src/app.py
@@ -3,16 +3,15 @@
 # app.py
 # Flask python web application
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import psycopg2
 import sys
 import os
 
 
 dbname = sys.argv[1]
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__), 'templates')
 
-app = Flask(__name__, template_folder='/templates')
+app = Flask(__name__, template_folder='templates')
 conn = psycopg2.connect(dbname=dbname, host='/tmp/')
 cur = conn.cursor()
 
@@ -22,7 +21,21 @@ print (cur.fetchall())
 # Login Page
 @app.route('/', methods=['GET','POST'])
 def login():
+    if (request.method == 'POST'):
+        un = request.form.get('username')
+        pw = request.form.get('password')
+
+        print("UN: " + un, "PW: " + pw)
+        return redirect('/home/' + un)
+
     return render_template('login.html')
+
+@app.route('/home/<string:username>', methods = ['GET','POST'])
+def home(username = "User"):
+    if (request.method == 'POST'):
+        print("PSOTED")
+    return render_template('home.html', username=username)
+
 
 app.run(host='0.0.0.0', port=8080)
 
