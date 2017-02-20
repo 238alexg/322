78f482423eda3c7575ae6426a5b055c84161771f
Alex Geoffrey <apg@uoregon.edu>
2017-02-20 02:41:23 +0000

    Working on app.py login

--- src/app.py
+++ src/app.py
@@ -8,13 +8,38 @@ cur = conn.cursor()
 
 # Login page
 @app.route('/', methods=['GET','POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     if (request.method == 'POST'):
         loginUN = request.form.get('username')
         pw = request.form.get('password')
-        return redirect('/home/' + loginUN)
-    return render_template('login.html')
+        
+        # If user provided username and password
+        if ((loginUN != None) & (pw != None)):
+            cur.execute("SELECT * FROM users WHERE users.username = " + loginUN + ";")
+            user = cur.fetchone()
+            if (user != None):
+                return render_template('login.html', error="User already exists!")
+            else:
+                cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (loginUN, password))
+                cur.commit()
+                return redirect('/home/' + loginUN)
 
-app.run(host='0.0.0.0', port=dbport)
+    else if (request.method == 'GET'):
+
+   
+   return render_template('login.html')
+
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    if (request.method == 'POST'):
+        # COPY FROM LOGIN
+    else:
+        redirect('/login')
+
+
+app.run(host='0.0.0.0', port='8080')
 
 
