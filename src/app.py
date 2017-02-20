303479842c2b18facab7d49d5a9bfeb1303588e8
Alex Geoffrey <apg@uoregon.edu>
2017-02-19 19:32:58 -0800

    Made all routes and template skeletons

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect
-from config import dbname, dbhost, dbport
+from flask import Flask, session, render_template, request, redirect
+from config import dbname, dbhost, dbport, secret_key
 import psycopg2
 
 app = Flask(__name__, template_folder='templates')
@@ -18,27 +18,50 @@ def login():
         if ((loginUN != None) & (pw != None)):
             cur.execute("SELECT * FROM users WHERE users.username = " + loginUN + ";")
             user = cur.fetchone()
+
+            # If username exists, check password
             if (user != None):
-                return render_template('login.html', error="User already exists!")
-            else:
-                cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (loginUN, password))
-                cur.commit()
-                return redirect('/home/' + loginUN)
+                # Password matches form data
+                if (user[2] == pw):
+                    session['username'] = loginUN
+                    print ("LOGGED IN " + user[1])
+                    return render_template('dashboard.html', username=loginUN)
+                else:
+                    return render_template('login.html', error="Incorrect password!")
 
     else if (request.method == 'GET'):
+        return render_template('login.html', error=" ")
 
-   
-   return render_template('login.html')
-
-
-
+# Route for creating new users
 @app.route('/create_user', methods=['POST'])
 def create_user():
     if (request.method == 'POST'):
-        # COPY FROM LOGIN
-    else:
-        redirect('/login')
+        loginUN = request.form.get('username')
+        pw = request.form.get('password')
+        
+        # If user provided username and password
+        if ((loginUN != None) & (pw != None)):
+            cur.execute("SELECT * FROM users WHERE users.username = " + loginUN + ";")
+            user = cur.fetchone()
 
+            # If username is available, create user or return with error
+            if (user != None):
+                return render_template('create_user.html', error="User already exists!")
+            else:
+                cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (loginUN, password))
+                cur.commit()
+                return redirect('dashboard.html', username=loginUN)
+    else:
+        return render_template('create_user.html', error=" ")
+
+@app.route('/dashboard', methods=['GET'])
+def dashboard():
+    return render_template('dashboard.html', user=escape(session['username']))
+
+@app.route('/logout', methods=['GET','POST'])
+def logout():
+    session.pop('username', None)
+    redirect('/login', error="Successfully logged out!")
 
 app.run(host='0.0.0.0', port='8080')
 
