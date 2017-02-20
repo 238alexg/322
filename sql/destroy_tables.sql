86c23397513a2ce5086304eccbf00a8e2f981cb8
Alex Geoffrey <apg@uoregon.edu>
2017-02-20 01:17:09 +0000

    Removing old project files

--- sql/destroy_tables.sql
+++
@@ -1,6 +0,0 @@
--- Alex Geoffrey
-
--- Code to destroy tables for testing
-
-drop schema public cascade;
-create schema public;
--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-cp -r src/templates ../../322/src
-cp src/app.py ../../322/src
-cp src/lost_config.json ../../322/src
-cp src/config.py ../../322/src
-
-echo "Transfer complete"
-
