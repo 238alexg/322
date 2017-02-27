f16214d59aacd7750921159707312a81503a0bd5
Alex Geoffrey <apg@uoregon.edu>
2017-02-27 01:00:49 -0800

    Commented app.py, updated README

--- src/README.txt
+++ src/README.txt
@@ -4,5 +4,10 @@
 -config.py: This file loads in the flask configurations from a JSON file in the wsgi directory
 -templates/
 	login.html: Presents the login page for users
-	create_user.html: Similar to login screen, except goes to the create_user route instead of the login.
+	create_user.html: Similar to login screen, except goes to the create_user route instead of the login. Also has a role field.
 	dashboard.html: Simple dashboard that greets the user with their username
+	add_asset.html: Allows user to input a new asset to the database
+	add_facility.html: Allows user to add a new facility to the database
+	asset_report.html: Loads/posts data generating asset reports for facilities/dates
+	dispose_asset.html: Allows qualified users to dispose of assets
+	error.html: A generic template feeding various errors to the user. Also links back to the dashboard.
--- src/app.py
+++ src/app.py
@@ -26,10 +26,8 @@ def login():
             # If username exists, check password
             if (user != None):
                 # Password matches form data
-                print("USER PW:" + user[2] + ", input: " + pw)
                 if (user[2] == pw):
                     session['username'] = loginUN
-                    print ("LOGGED IN " + session['username'])
                     return redirect('/dashboard')
                 else:
                     return render_template('login.html', error="Incorrect password!")
@@ -92,24 +90,28 @@ def dashboard():
 # Route to add a facility
 @app.route('/add_facility', methods=['GET','POST'])
 def add_facility(error=""):
+    # If just visiting page
     if (request.method == 'GET'):
-        # Template with form that allows name, fcode, and any other attributes to be entered
-        # Have post action but stay on this route
-        # List all facilities currently in the DB, preferably before form
-        
+        # Get all existing facilities form the DB
         cur.execute("SELECT * FROM facilities;")
         facilities = cur.fetchall()
-
+        # Load them into the page
         return render_template('add_facility.html', facilities=facilities, error=error)
+    
+    # If adding a facility
     elif (request.method == 'POST'):
+        # Get form data
         facilityName = request.form.get('facilityName')
         facilityCode = request.form.get('facilityCode')
 
+        # Get the facility with that name
         cur.execute("SELECT * FROM facilities WHERE (name = \'" + facilityName + "\' OR code = \'" + facilityCode + "\')")
         facility = cur.fetchone()
 
+        # Facility already exists
         if (facility != None):
-            return render_template('duplicateEntry.html')
+            return render_template('error.html',error="Facility already exists!")
+        # If facility doesn't exist yet, add to the DB
         else:
             cur.execute("INSERT INTO facilities (name, code) VALUES (%s, %s)", (facilityName, facilityCode))
             conn.commit()
@@ -118,26 +120,32 @@ def add_facility(error=""):
 # Route to add an asset
 @app.route('/add_asset', methods=['GET','POST'])
 def add_asset():
+    # If visiting page
     if (request.method == 'GET'):
+        # Get all the facility names
         cur.execute("SELECT name FROM facilities;")
         facilityNames = cur.fetchall()
-
+        # And load them into the page
         return render_template('add_asset.html', facilities=facilityNames)
+    
+    # If trying to add an asset
     elif (request.method == 'POST'):
+        # Get form data
         assetTag = request.form.get('assetTag')
         assetDesc = request.form.get('assetDesc')
         facilityName = request.form.get('facility')
-        
         rawtime = request.form.get('date')
-        print("RT: " + rawtime)
         dtobj = datetime.strptime(rawtime, "%Y-%m-%d" + "T" + "%H:%M")
-        print(dtobj)
 
+        # Get asset with same tag, if any
         cur.execute("SELECT * FROM assets WHERE tag = \'" + assetTag + "\'")
         asset = cur.fetchone()
 
+        # If found asset with the same tag already, error
         if (asset != None):
-            return render_template('duplicateEntry.html')
+            return render_template('error.html', error="Asset with tag " + assetTag + " already exists!")
+        
+        # Otherwise add this asset to the DB
         else: 
             cur.execute("SELECT facility_pk FROM facilities WHERE facilities.name = \'" + facilityName + "\'")
             facilityFK = cur.fetchone()
@@ -148,52 +156,77 @@ def add_asset():
 # Route to dispose of an asset
 @app.route('/dispose_asset', methods=['GET','POST'])
 def dispose_asset():
+    # Get logged in user's UN
     loginUN = session['username']
 
+    # Find user's role in the DB
     cur.execute("SELECT roles.rolename FROM users, roles WHERE (users.username = \'" + loginUN + "\' AND users.role_fk = roles.roles_pk)")
     role = cur.fetchone()
+
+    # If they don't have the correct role, error
     if (role[0] != "Logistics Officer"):
         return render_template("error.html", error="User's role must be Losgistics Officer in order to modify assets")
+    
+    # If visiting page, load vanilla page
     elif (request.method == 'GET'):
         return render_template('dispose_asset.html')
+    
+    # If trying to dispose of an asset
     elif (request.method == 'POST'):
+        # Get form data
         tag = request.form.get('assetTag')
         rawdt = request.form.get('date')
-
         dtobj = datetime.strptime(rawdt, "%Y-%m-%d" + "T" + "%H:%M")
+
+        # Find asset to dispose of
         cur.execute("SELECT * FROM assets WHERE assets.tag = \'" + tag + "\'")
         asset = cur.fetchone()
+
+        # If asset found
         if (asset != None):
+            # If it is already disposed of, error
             if (asset[3] == None):
                 return render_template('error.html', error="Asset already disposed")
+            # Else, update with disposed facility (NULL) and form date
             else:
                 cur.execute("UPDATE assets SET facility_fk = NULL, arrival_dt = %s WHERE assets.tag = %s", (dtobj, tag))
+        # Asset not found, error
         else:
             return render_template('error.html', error="Asset does not exist!")
 
+        # Save changes and redirect to dashboard
         conn.commit()
         return redirect('/dashboard')
 
 # Route to report assets from a given day
 @app.route('/asset_report', methods=['GET','POST'])
 def asset_report():
+    # Get facility names for loading into page
     cur.execute("SELECT facilities.name FROM facilities")
     facilityNames = cur.fetchall()
 
+    # If visiting page, load page
     if (request.method == 'GET'):
         return render_template('asset_report.html', facilities=facilityNames)
+    
+    # If generating a report
     elif (request.method == 'POST'):
+        # Get form data
         facility = request.form.get('facility')
         rawdate = request.form.get('date')
         dtobj = datetime.strptime(rawdate, "%Y-%m-%d" + "T" + "%H:%M")
 
+        # If no facility indicated, load all assets from DB where arrival date is BEFORE date on form (already arrived)
         if (facility == "All"):
             cur.execute("SELECT * FROM assets WHERE assets.arrival_dt <= %s", [dtobj])
+        
+        # Else do above only with assets from given facility
         else:
             cur.execute("SELECT facilities.facility_pk FROM facilities WHERE facilities.name = \'" + facility + "\'")
             fpk = cur.fetchone()
             cur.execute("SELECT * FROM assets WHERE (assets.arrival_dt <= %s AND assets.facility_fk = %s)", (dtobj, fpk))
 
+        # Load report results and facility names into the page
         assets = cur.fetchall()
         return render_template('asset_report.html', assets=assets, facilities=facilityNames)
 
@@ -203,6 +236,7 @@ def logout():
     session.pop('username', None)
     return render_template('/login.html', error="Successfully logged out!")
 
+# When not using mod-wsgi, uncomment below
 #app.run(host='0.0.0.0', port='8080')
 
 
