82165dceb307caa6171481dd678e092be4265f19
Alex Geoffrey <apg@uoregon.edu>
2017-02-27 00:45:16 +0000

    Updated preflight

--- preflight.sh
+++ preflight.sh
@@ -5,6 +5,10 @@ cd sql
 psql $1 -f create_tables.sql
 cd ..
 
+# Copies all files to the /wsgi directory
 cp -R src/* $HOME/wsgi
 
+# Comment out before submission!
+# Restarts server
+apachectl restart
 
