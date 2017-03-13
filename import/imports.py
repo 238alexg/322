f2573df88f4524a3853ea96e39cb4f9e784c78a8
Alex Geoffrey <apg@uoregon.edu>
2017-03-13 02:26:08 -0700

    Commented more

--- import/imports.py
+++ import/imports.py
@@ -11,30 +11,38 @@ import csv
 conn = psycopg2.connect(dbname = sys.argv[1], host="127.0.0.1")
 cur = conn.cursor()
 
+# IMPORT USERS
 firstline = True
 with open(sys.argv[2] + "/users.csv") as userCSV:
     reader = csv.reader(userCSV, delimiter=',')
     for row in reader:
+        # Do not include title row
         if (firstline):
             firstline = False
             continue
         
+        # Get user role
         cur.execute("SELECT * FROM roles WHERE roles.rolename = '" + row[2] + "'")
         role = cur.fetchone()
 
+        # Insert role if it doesn't exist
         if (role == None):
             cur.execute("INSERT INTO roles (rolename) VALUES ('" + row[2] + "')")
             cur.execute("SELECT * FROM roles WHERE roles.rolename = '" + row[2] + "'")
             role = cur.fetchone() 
         
+        # Once role exists in DB, insert user
         cur.execute("INSERT INTO users (username, password, role_fk, isActive) VALUES (%s, %s, %s, %s)",(row[0], row[1], role[0], row[3]))
 
 conn.commit()
 
+
+# IMPORT FACILITIES
 firstline = True
 with open(sys.argv[2] + "/facilities.csv") as facCSV:
     reader = csv.reader(facCSV, delimiter=',')
     for row in reader:
+        # Do not include title row
         if (firstline):
             firstline = False
             continue
@@ -43,17 +51,22 @@ with open(sys.argv[2] + "/facilities.csv") as facCSV:
 
 conn.commit()
 
+
+# IMPORT ASSETS
 firstline = True
 with open(sys.argv[2] + "/assets.csv") as assetCSV:
     reader = csv.reader(assetCSV, delimiter=',')
     for row in reader:
+        # Do not include title row
         if (firstline):
             firstline = False
             continue
 
+        # Get facility key
         cur.execute("SELECT facility_pk FROM facilities WHERE facilities.code = '" + row[2] + "'")
         fac = cur.fetchone()
         
+        # If dispose time is not "NULL", pad with single quotes for DB insertion
         if (row[4] != "NULL"):
             aquire_dt = "'" + row[4] + "'"
         else:
@@ -63,17 +76,23 @@ with open(sys.argv[2] + "/assets.csv") as assetCSV:
 
 conn.commit()
 
+
+
+# IMPORT TRANSFERS
 firstline = True
 with open(sys.argv[2] + "/transfers.csv") as transCSV:
     reader = csv.reader(transCSV, delimiter=',')
     for row in reader:
+        # Do not include title row
         if (firstline):
             firstline = False
             continue
         
+        # Find asset
         cur.execute("SELECT assets.assets_pk FROM assets WHERE assets.tag = '" + row[0] + "'")
         asset = cur.fetchone()
 
+        # Find requester and approver (if not "NULL")
         if (row[3] != "NULL"):
             cur.execute("SELECT r.user_pk, a.user_pk FROM users JOIN users r ON r.username = '" + row[1] + "' JOIN users a ON a.username = '" + row[3] + "'")
             users = cur.fetchone()
@@ -85,6 +104,7 @@ with open(sys.argv[2] + "/transfers.csv") as transCSV:
             reqUser = users[0]
             appUser = row[3]
 
+        # Pad optional datetimes with single quotes if they are not null
         if (row[4] != "NULL"):
             approve_dt = "'" + row[4] + "'"
         else:
@@ -100,6 +120,7 @@ with open(sys.argv[2] + "/transfers.csv") as transCSV:
         else:
             unload_dt = row[8]
 
+        # Find souce and destination facilities
         cur.execute("SELECT s.facility_pk, d.facility_pk FROM facilities JOIN facilities s ON s.code = '" + row[5] + "' JOIN facilities d ON d.code = '" + row[6] + "'")
         facs = cur.fetchone()
         source = facs[0]
